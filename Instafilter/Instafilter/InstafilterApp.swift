51759627be3b5ac4e329dc9f24a555c6342abffd
Larry N <larryn35@gmail.com>
2020-09-23 01:02:14 -0700

    initial commit

---
+++ Instafilter/Instafilter/InstafilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstafilterApp.swift
+//  Instafilter
+//
+//  Created by Larry Nguyen on 9/23/20.
+//
+
+import SwiftUI
+
+@main
+struct InstafilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
