e934bfb77d609ce0414cf8255ddc9e5206b56cf9
Larry N <larryn35@gmail.com>
2020-09-23 16:55:31 -0700

    save edited photos

--- Instafilter/Instafilter/ContentView.swift
+++ Instafilter/Instafilter/ContentView.swift
@@ -16,6 +16,7 @@ struct ContentView: View {
     @State private var showingImagePicker = false
     @State private var showingFilterSheet = false
     @State private var inputImage: UIImage?
+    @State private var processedImage: UIImage?
     
     @State private var currentFilter: CIFilter = CIFilter.sepiaTone()
     let context = CIContext()
@@ -37,6 +38,7 @@ struct ContentView: View {
         if let cgimg = context.createCGImage(outputImage, from: outputImage.extent) {
             let uiImage = UIImage(cgImage: cgimg)
             image = Image(uiImage: uiImage)
+            processedImage = uiImage
         }
     }
     
@@ -94,6 +96,16 @@ struct ContentView: View {
                     
                     Button("Save") {
                         // save the picture
+                        guard let processedImage = self.processedImage else { return }
+                        
+                        let imageSaver = ImageSaver()
+                        imageSaver.successHandler = {
+                            print("Success")
+                        }
+                        imageSaver.errorHandler = {
+                            print("Oops: \($0.localizedDescription)")
+                        }
+                        imageSaver.writeToPhotoAlbum(image: processedImage)
                     }
                 }
             }
